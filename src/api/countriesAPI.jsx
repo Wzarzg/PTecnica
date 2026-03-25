c02092efd4923ffc49cebc795d68622d7403cfdf
Wzarzg <wdiegozarate@gmail.com>
2026-03-25 11:10:11 -0500

    fix: :bug: enlace para detalle de paises
    mejora de enlace para exactitud

--- src/api/countriesAPI.jsx
+++ src/api/countriesAPI.jsx
@@ -11,7 +11,7 @@ export const carga = async () => {
 
 export const cargaDetalle = async(name)=>{
     const res = await axios.get(
-        `https://restcountries.com/v3.1/name/${name}`
+        `https://restcountries.com/v3.1/name/${name}?fullText=true`
     )
     return res.data[0]
 }
